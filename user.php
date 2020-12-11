9ada28f312ccb7c362ce7935f2260d608ecf675e
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2020-12-11 09:20:40 +0100

    job 01 ok

--- user.php
+++ user.php
@@ -240,4 +240,47 @@ class user
         exit;
     }
 
-}?>
+}
+
+
+//création d'un nouvel user avec 'new'
+$marie = new user();
+//enregistrer les infos du nouvel user
+//$marie->register('marielolabc', 'mariepass','marie.clerc@laplateforme.io','marie', 'clerc');
+//dump les infos dans un tableau
+//var_dump($marie);
+
+//-------------------------------------------------------------------------
+
+$marie->connect('marieM', 'pass');
+
+//dump les infos dans un tableau
+
+
+//$marie->disconnect();
+
+//$marie->delete();
+
+$marie->update('marie', 'pass','marie.clerc@laplateforme.io','MMarie', 'clerc');
+
+//$marie->isConnected();
+
+//$marie->getAllInfos();
+
+//$marie->getLogin();
+
+//$marie->getEmail();
+
+//$marie->getFirstname();
+
+//$marie->getLastname();
+
+$marie->refresh();
+
+
+var_dump($marie);
+
+
+
+
+?>
